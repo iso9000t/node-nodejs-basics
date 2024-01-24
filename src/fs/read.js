2d9c579b34c19ebc496c57369ebddfe5a4457def
iso9000t <sasha.gr81@gmail.com>
2024-01-24 19:33:11 -0300

    feat: add read function logic

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const fileToRead = join(__dirname, "files", "fileToRead.txt");
+  try {
+    const data = await readFile(fileToRead, "utf8");
+    console.log(data);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
