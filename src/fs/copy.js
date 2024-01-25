67e74772719c8165925eff1b93cee703f10c188e
iso9000t <sasha.gr81@gmail.com>
2024-01-25 02:19:22 -0300

    feat: add copy function logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import { access, copyFile, mkdir, readdir } from "node:fs/promises";
+import { constants } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const sourceDirPath = join(__dirname, "files");
+  const destinationDirPath = join(__dirname, "files_copy");
+
+  try {
+    await access(sourceDirPath, constants.R_OK);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await access(destinationDirPath, constants.R_OK);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      throw error;
+    }
+  }
+
+  await mkdir(destinationDirPath);
+  const files = await readdir(sourceDirPath);
+  for (const file of files) {
+    const sourcePath = join(sourceDirPath, file);
+    const destinationPath = join(destinationDirPath, file);
+    await copyFile(sourcePath, destinationPath);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -8,7 +8,7 @@ const create = async () => {
     const filePath = join(__dirname, 'files', 'fresh.txt')
     try {
         await access(filePath, constants.F_OK);
-        throw new Error("FS operation failed");
+        throw new Error('FS operation failed');
     } catch (error) {
         if (error.code === 'ENOENT') {
             await writeFile(filePath, 'I am fresh and young');
