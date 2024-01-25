90ad5df8d79f0844beb25355ec4afa5e9c47e864
iso9000t <sasha.gr81@gmail.com>
2024-01-25 01:00:35 -0300

    feat: add create function logic

--- src/fs/read.js
+++ src/fs/read.js
@@ -4,12 +4,12 @@ import { fileURLToPath } from "node:url";
 
 const read = async () => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
-  const fileToRead = join(__dirname, "files", "fileToRead.txt");
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
   try {
-    const data = await readFile(fileToRead, "utf8");
+    const data = await readFile(filePath, 'utf8');
     console.log(data);
   } catch (error) {
-    throw new Error("FS operation failed");
+    throw new Error('FS operation failed');
   }
 };
 
