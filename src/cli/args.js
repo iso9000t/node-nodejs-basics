2866ef80795e4acf45e2c4cc7347733e85dd5a10
iso9000t <sasha.gr81@gmail.com>
2024-01-26 16:36:45 -0300

    feat: add parseArgs function logic

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  for (let i = 0; i < args.length; i++) {
+    if (i % 2 === 0) {
+      const name = args[i].replace('--', '');
+      const value = args[i + 1];
+      console.log(`${name} is ${value}`);
+    }
+  }
 };
 
 parseArgs();
