454f0ad750ad8ae5cec709b584655db250a9fbcd
iso9000t <sasha.gr81@gmail.com>
2024-01-26 00:47:14 -0300

    feat add remove and list functions logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { access, unlink } from 'node:fs/promises';
+import { constants } from 'node:fs';
+import { join, dirname } from "node:path";
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const deleteFilePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await access(deleteFilePath, constants.F_OK);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
+   
+    await unlink(deleteFilePath);
 };
 
 await remove();
