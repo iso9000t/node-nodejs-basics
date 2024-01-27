2f7ab0d7b7191c8ae95c973fef1efa9f799b886a
iso9000t <sasha.gr81@gmail.com>
2024-01-27 04:48:27 -0300

    feat: add read and write fuction logic for streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  const readStream = createReadStream(filePath);
+
+  try {
+    await pipeline(readStream, process.stdout);
+    console.log('Read stream complete.');
+  } catch (error) {
+    console.error('Error in read stream:', error);
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+  const writeStream = createWriteStream(filePath);
+
+  try {
+    await pipeline(process.stdin, writeStream);
+    console.log('Write stream complete.');
+  } catch (error) {
+    console.error('Error in write stream:', error);
+  }
 };
 
 await write();
