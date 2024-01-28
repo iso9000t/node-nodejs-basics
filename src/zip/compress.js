759344f0f1a0850d464d1fce36ba803e15da2941
iso9000t <sasha.gr81@gmail.com>
2024-01-28 03:05:20 -0300

    feat: add calculateHash, compress and decompress functions logic

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const txtFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+  const gzFilePath = join(__dirname, 'files', 'archive.gz');
+
+  const gzip = createGzip();
+  const sourceStream = createReadStream(txtFilePath);
+  const destinationStream = createWriteStream(gzFilePath);
+
+  try {
+    await pipeline(sourceStream, gzip, destinationStream);
+    console.log(
+      'File successfully compressed');
+  } catch (error) {
+    error.code === 'ENOENT'
+      ? console.log('File not found')
+      : console.log('Error in compressing file', error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { createGunzip } from 'node:zlib';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const gzFilePath = join(__dirname, 'files', 'archive.gz');
+  const txtFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+
+  const gunzip = createGunzip();
+  const sourceStream = createReadStream(gzFilePath);
+  const destinationStream = createWriteStream(txtFilePath);
+
+  try {
+    await pipeline(sourceStream, gunzip, destinationStream);
+    console.log('File successfully decompressed');
+  } catch (error) {
+    error.code === 'ENOENT'
+      ? console.log('File not found')
+      : console.log('Error in decompressing file', error);
+  }
 };
 
 await decompress();
