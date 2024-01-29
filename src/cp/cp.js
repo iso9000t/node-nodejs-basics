8e17bdeadb09db48bef279ef25a8a1361e1840ad
iso9000t <sasha.gr81@gmail.com>
2024-01-29 01:20:22 -0300

    feat: add spawnChildProcess function logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+   const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'script.js');
+
+    const childProcess = fork(filePath, args, {silent: true});
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
